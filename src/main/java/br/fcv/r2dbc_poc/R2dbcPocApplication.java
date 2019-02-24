8137d17550bee7c8f34e5ca60b9167acca7cd5a2
Fabio Veronez <fabio.veronez@gmail.com>
2019-02-24 13:27:10 +0100

    Add initial project structure
    Add initial project structure created based on [Spring Initializr](https://start.spring.io/)

---
+++ src/main/java/br/fcv/r2dbc_poc/R2dbcPocApplication.java
@@ -0,0 +1,13 @@
+package br.fcv.r2dbc_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fcv/r2dbc_poc/R2dbcPocApplicationTests.java
@@ -0,0 +1,16 @@
+package br.fcv.r2dbc_poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class R2dbcPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
